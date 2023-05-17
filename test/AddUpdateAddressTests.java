ce0d61e582a654d8ebf0ac326735ff2fae773435
priyanka-997 <priyanka997sharma@gmail.com>
2023-05-17 19:44:55 +0530

    two classes updated

--- test/AddUpdateAddressTests.java
+++ test/AddUpdateAddressTests.java
@@ -7,12 +7,14 @@ import io.restassured.response.Response;
 
 public class AddUpdateAddressTests {
 	
-	public static String[] addUpdateAddressPayload = {"house_no", "A-53", "street_no", "24", "locality", "Noida City Center",
-			"pincode", "201301", "city", "Noida", "address_type", "Home", "state", "Uttar Pradesh"};
+	
 
 			
 	@Test(priority=9)
 	public static void testAddUpdateAddress() {
+		
+		String[] addUpdateAddressPayload = {"house_no", "A-53", "street_no", "24", "locality", "Noida City Center",
+				"pincode", "201301", "city", "Noida", "address_type", "Home", "state", "Uttar Pradesh"};
 		
 		Response response = AddUpdateAddressEndPoints.addUpdateAddress(addUpdateAddressPayload);
 				response.then().log().all();
